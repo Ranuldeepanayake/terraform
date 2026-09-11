9c0e96d6bbf62208c644232efcbd75f413d0e741
Ranul Deepanayake <ranuldeepanayake@outlook.com>
2026-09-11 12:57:19 +0530

    feat: created a caller for the EKS platform module

--- aws/module-instances/eks/platform/main.tf
+++ aws/module-instances/eks/platform/main.tf
@@ -1,27 +1,27 @@
 locals {
-  aws_region     = "ap-southeast-1"
+  aws_region                 = "ap-southeast-1"
+  eks_cluster_workspace_name = "aws-dev-eks-cluster"
+}
+
+data "terraform_remote_state" "eks_cluster" {
+  backend = "remote"
+
+  config = {
+    organization = "ranuldeepanayake"
+
+    workspaces = {
+      name = local.eks_cluster_workspace_name
+    }
+  }
 }
 
 data "aws_eks_cluster" "this" {
-  name = "eks-cluster"
-}
-
-# Can be removed after checking.
-data "aws_eks_cluster_auth" "this" {
-  name = data.aws_eks_cluster.this.name
+  name = data.terraform_remote_state.eks_cluster.outputs.cluster_id
 }
 
 # Create a module for this. Use the standalone EKS for everything else for now.
 module "eks_platform" {
   source = "../../../modules/eks-platform"
 
-  eks_cluster_workspace_name = "aws-dev-eks-cluster"
-  letsencrypt_email_address  = "ranuldeepanayake@outlook.com"
-
-  route53_zones = {
-    ranul_click = {
-      dns_zone = "ranul.click"
-      zone_id  = "Z0233745IB90EWVKK2K1"
-    }
-  }
+  eks_cluster_workspace_name = local.eks_cluster_workspace_name
 }
