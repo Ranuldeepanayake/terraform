ee99d1365813fe26102aac391fa160935ad67775
Ranul Deepanayake <ranuldeepanayake@outlook.com>
2026-09-25 11:04:48 +0530

    feat: subnet module caller supports IPv6

--- aws/module-instances/subnet-private/main.tf
+++ aws/module-instances/subnet-private/main.tf
@@ -40,10 +40,12 @@ module "subnet" {
 
   subnets = {
     a = {
-      cidr_block              = "10.0.3.0/24"
-      availability_zone       = "ap-southeast-1a"
-      map_public_ip_on_launch = false
-      route_table_id          = data.terraform_remote_state.route_table.outputs.route_table_id
+      cidr_block                      = "10.0.3.0/24"
+      ipv6_cidr_block                 = null
+      assign_ipv6_address_on_creation = false
+      availability_zone               = "ap-southeast-1a"
+      map_public_ip_on_launch         = false
+      route_table_id                  = data.terraform_remote_state.route_table.outputs.route_table_id
       tags = {
         Name = "${local.project_name}_subnet-c"
         az   = "ap-southeast-1a"
@@ -51,10 +53,12 @@ module "subnet" {
     }
 
     b = {
-      cidr_block              = "10.0.4.0/24"
-      availability_zone       = "ap-southeast-1b"
-      map_public_ip_on_launch = false
-      route_table_id          = data.terraform_remote_state.route_table.outputs.route_table_id
+      cidr_block                      = "10.0.4.0/24"
+      ipv6_cidr_block                 = null
+      assign_ipv6_address_on_creation = false
+      availability_zone               = "ap-southeast-1b"
+      map_public_ip_on_launch         = false
+      route_table_id                  = data.terraform_remote_state.route_table.outputs.route_table_id
       tags = {
         Name = "${local.project_name}_subnet-d"
         az   = "ap-southeast-1b"
