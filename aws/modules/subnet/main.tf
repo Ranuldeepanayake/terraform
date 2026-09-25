7df2ddc7b9ea7417c1b8b60fef8b421f3d8b5b6e
Ranul Deepanayake <ranuldeepanayake@outlook.com>
2026-09-25 11:03:51 +0530

    feat: subnet module supports IPv6

--- aws/modules/subnet/main.tf
+++ aws/modules/subnet/main.tf
@@ -1,10 +1,12 @@
 resource "aws_subnet" "subnet" {
   for_each = var.subnets
 
-  vpc_id                  = var.vpc_id
-  cidr_block              = each.value.cidr_block
-  availability_zone       = each.value.availability_zone
-  map_public_ip_on_launch = each.value.map_public_ip_on_launch
+  vpc_id                          = var.vpc_id
+  cidr_block                      = each.value.cidr_block
+  ipv6_cidr_block                 = each.value.ipv6_cidr_block
+  assign_ipv6_address_on_creation = each.value.assign_ipv6_address_on_creation
+  availability_zone               = each.value.availability_zone
+  map_public_ip_on_launch         = each.value.map_public_ip_on_launch
 
   tags = merge(
     var.tags,
--- aws/modules/subnet/variables.tf
+++ aws/modules/subnet/variables.tf
@@ -1,18 +1,20 @@
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
 variable "subnets" {
   description = "Public subnets to create"
 
   type = map(object({
-    cidr_block              = string
-    availability_zone       = string
-    map_public_ip_on_launch = bool
-    route_table_id          = string
-    tags                    = map(string)
+    cidr_block                      = string
+    ipv6_cidr_block                 = optional(string)
+    assign_ipv6_address_on_creation = optional(bool, false)
+    availability_zone               = string
+    map_public_ip_on_launch         = bool
+    route_table_id                  = string
+    tags                            = map(string)
   }))
-}
-
-variable "vpc_id" {
-  description = "VPC ID"
-  type        = string
 }
 
 variable "tags" {
